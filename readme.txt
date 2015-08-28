92d8e22fd7c34bfe55f2c54dbd881b243d570502
Danilo Righetto <danilo.righetto@fornax.com.br>
2015-08-28 10:36:47 -0300

    Trabalhando com os metodos: GET e POST

---
+++ readme.txt
@@ -0,0 +1,8 @@
+Projeto: Gerenciador
+Fonte: Curso da Alura - Servlet 3 e Fundamentos da Web
+Nome do Projeto: gerenciador
+IDE utilizada: Eclipse Luna
+Versão do Java: 1.7
+
+Autor: Danilo Righetto
+Email: danilorighetto@hotmail.com
--- src/main/java/br/com/alura/gerenciador/web/NovaEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/web/NovaEmpresa.java
@@ -22,13 +22,18 @@ public class NovaEmpresa extends HttpServlet {
 	/* (non-Javadoc)
 	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
 	 * Dentro do nosso doGet estamos adicionando uma NovaEmpresa
+	 * Porem o GET nao e o melhor metodo para o nosso uso pois o GET possui limite de envio para o servidor
+	 * Por isso vamos trocar o doGet para doPost
 	 */
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String nome = req.getParameter("nome");
 		Empresa empresa = new Empresa(nome);
 		new EmpresaDAO().adiciona(empresa);
 		PrintWriter writer = resp.getWriter();
-		writer.println("<html><body>Empresa " + empresa.getNome() +" Adicionada com Sucesso</body></html>");
+		writer.println("<html><body>Empresa Adicionada com Sucesso: "+ empresa.getNome() +"</body></html>");
 	}
+	/* O HTTP suporta alem do GET e do POST os metodos:
+	 * delete, head, trace entre outros.
+	 */
 }
